80bd46da418f718a8fa777503326321a725c10db
JeffWiedrich2 <55073690+JeffWiedrich2@users.noreply.github.com>
2021-01-18 23:12:45 -0700

    Add files via upload

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+	public GameObject gameOver;
+	public Rigidbody2D rb;
+
+	Vector2 movement;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+		gameOver.SetActive(true);
+    }
+
+    void FixedUpdate()
+    {
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public Text countdownText;
+    public GameObject youWin;
+
+    private float countdown = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            WinGame();
+        }
+
+        countdown -= Time.deltaTime;
+
+        countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+
+        countdownText.text = string.Format("{0:00.00}", countdown);
+    }
+
+    void WinGame()
+    {
+        youWin.SetActive(true);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
